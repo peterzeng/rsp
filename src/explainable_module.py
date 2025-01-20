ffc2c8904886f0390c44e205036c0301d2a4e254
peterzeng <peterateast@gmail.com>
2025-01-20 11:37:42 -0500

    updated explainable module and train.py

--- src/explainable_module.py
+++ src/explainable_module.py
@@ -1,7 +1,14 @@
 from gram2vec import vectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pickle
+import time
 import os
 
+'''
+    This module is used to get the vector representation of a document and the cosine similarity between two documents.
+    We assume the cache is not normalized unless otherwise specified.
+'''
 class ExplainableModule():
     def __init__(self) -> None:
         pass
@@ -10,31 +17,44 @@ class ExplainableModule():
         pass
 
 class Gram2VecModule(ExplainableModule):
-    def __init__(self, dataset) -> None:
+    def __init__(self, filepath, dataset, save_dir, run_id, configs) -> None:
         super().__init__()
         self.dataset = dataset
-        if not os.path.exists(f"{dataset}_vector_map.pkl"):
+        self.save_dir = save_dir
+        self.run_id = run_id
+
+        if not os.path.exists(filepath):
+            print(f"Cache for {filepath} not found, creating new cache.")
             self.cache = {}
         else:
-            import pickle
-            with open(f"{dataset}_vector_map.pkl", "rb") as f:
+            with open(filepath, "rb") as f:
                 self.cache = pickle.load(f)
 
-        self.vectorizer_configs = {
-            "pos_unigrams":1,
-            "pos_bigrams":1,
-            "func_words":1,
-            "punctuation":1,
-            "letters":1,
-            "emojis":1,
-            "dep_labels":1,
-            "morph_tags":1,
-            "sentences":1
-        }
+        if not configs:
+            self.vectorizer_configs = {
+                "pos_unigrams":1,
+                "pos_bigrams":1,
+                "func_words":1,
+                "punctuation":1,
+                "letters":0,
+                "emojis":1,
+                "dep_labels":1,
+                "morph_tags":1,
+                "sentences":1,
+                "num_tokens":0
+            }
+        else:
+            self.vectorizer_configs = configs
 
-    def cache_features(self, u_id, vector): 
-        self.cache[u_id] = vector
-    
+    def cache_features(self, uid, vector): 
+        if uid not in self.cache:
+            self.cache[uid] = vector
+
+    def cache_vector(self, doc, uid):
+        if uid not in self.cache:
+            vector = vectorizer.from_documents([doc], config = self.vectorizer_configs).values
+            self.cache_features(uid, vector)
+
     def get_vector(self, doc, uid):
         if uid in self.cache:
             return self.cache[uid]
@@ -43,24 +63,37 @@ class Gram2VecModule(ExplainableModule):
             self.cache_features(uid, vector)
             return vector
 
-    def get_vector_and_score(self, doc1, doc2, uid1='', uid2=''):
-
-        if uid1 in self.cache: # This is for during TA2 eval when we have computed the query vector already
-            vector_1 = self.cache[uid1]
+    # Can only be ran after vector cache is created.
+    def get_vector_and_score(self, doc1, doc2, uid1='', uid2='', normalized=False):
+        if normalized and not self.normalized_cache:
+            print("normalized cache not found, creating normalized_cache")
+            self.normalize_cache()
+            cache = self.normalized_cache
         else:
-            vector_1 = vectorizer.from_documents([doc1], config = self.vectorizer_configs).values
-            self.cache_features(uid1, vector_1)
+            cache = self.cache
 
-        if uid2 in self.cache:
-            vector_2 = self.cache[uid2]
-        else:
-            vector_2 = vectorizer.from_documents([doc2], config = self.vectorizer_configs).values
-            self.cache_features(uid2, vector_2)
+        try:
+            vector_1 = cache[uid1]
+        except KeyError:
+            print(f"uid1 {uid1} not in cache")
+
+        try:
+            vector_2 = cache[uid2]
+        except KeyError:
+            print(f"uid2 {uid2} not in cache")
 
         cosine_sim = cosine_similarity(vector_1, vector_2)[0][0]
         return vector_1, vector_2, cosine_sim
 
     def save_cache(self):
-        import pickle
-        with open(f"{self.dataset}_vector_map.pkl", "wb") as f:
+        with open(f"{self.save_dir}/{self.dataset}_{self.run_id}_vector_map.pkl", "wb") as f:
             pickle.dump(self.cache, f)
+
+    def normalize_cache(self):
+        start_time = time.time()
+        self.normalized_cache = {}
+        vector_std = np.std(list(self.cache.values()))
+        vector_mean = np.mean(list(self.cache.values()))
+        for uid in self.cache:
+            self.normalized_cache[uid] = (self.cache[uid] - vector_mean) / vector_std
+        print(f"Normalized cache in {time.time() - start_time:.2f} seconds")
--- src/train_attention_residual.py
+++ src/train_attention_residual.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from AttentionResidualModel import AttentionResidualDataset, AttentionResidualModel
 import pandas as pd
 from torch.optim import AdamW
-from explainable_module import Gram2VecModule
+from transformers import AutoModel
 from tqdm.auto import tqdm
 import logging
 from sklearn.metrics.pairwise import cosine_similarity
@@ -18,44 +18,110 @@ import numpy as np
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-def process_posts(post_df, g2v_vectorizer):
+def process_posts(post_df, g2v_vectorizer, normalized=False):
     data = []
     for i, row in tqdm(post_df.iterrows(), total=post_df.shape[0], desc="Processing Posts"):
-        features1, features2, cosim = g2v_vectorizer.get_vector_and_score(row['post_1'], row['post_2'], row['post_1_id'], row['post_2_id'])
-        residual = row['same'] - cosim
-        data.append({"text1":row['post_1'],
-                        "text2":row['post_2'],
+        features1, features2, cosim = g2v_vectorizer.get_vector_and_score(row['document1'], row['document2'], row['doc_id1'], row['doc_id2'], normalized=normalized)
+        residual = (1 - cosim) if row['same'] == 1 else (-1 - cosim)
+        data.append({"text1":row['document1'],
+                        "text2":row['document2'],
                         "features1":features1,
                         "features2":features2,
                         "residual":residual})
     
     return data
 
+def generate_neural_feature_map(dataloader, model_type):
+    if model_type == "/home/zengpe/rsp/LUAR-RU":
+        model = AutoModel.from_pretrained(model_type, local_files_only=True, trust_remote_code=True)
+    else:
+        model = AutoModel.from_pretrained(model_type, trust_remote_code=True)
+
+    model.to(device)
+    neural_cosims = []
+    model.eval()
+    with torch.no_grad():
+        dataloader = tqdm(dataloader, desc="test loop", position=1 ,leave=True)
+
+        for batch in dataloader:
+            with autocast():
+                doc1 = batch['text1']
+                doc2 = batch['text2']
+                doc1 = {k: v.to(device) for k, v in doc1.items()}
+                doc2 = {k: v.to(device) for k, v in doc2.items()}
+                outputs1 = model(**doc1).detach().cpu().numpy()
+                outputs2 = model(**doc2).detach().cpu().numpy()
+                
+            cosims = cosine_similarity(outputs1, outputs2)
+            neural_cosims.extend(cosims.diagonal())
+
+    model.cpu()
+    del model
+    torch.cuda.empty_cache()
+    return neural_cosims
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a model on residual data.")
-    parser.add_argument("-m", "--model_type", type=str, default="roberta", choices=["roberta", "roberta-large","luar"], help="Type of model to use for training.")
-    parser.add_argument("-d", "--dataset", type=str, default="reddit", choices=["fanfiction", "reddit", "amazon", "imbalanced_reddit"], help="Dataset to use for training.")
-    parser.add_argument("-r", "--run_id", type=str, default="0", help="Run ID for the experiment.")
+    parser.add_argument("-m", "--model_type", type=str, default="luar", choices=["roberta", "roberta-large", "luar", "style", "luar-ru"], help="Type of model to use for training.")
+    parser.add_argument("-d", "--dataset", type=str, default="reddit", choices=["fanfiction", "reddit", "amazon", "hiatus", "imbalanced_reddit","hiatus_combined"], help="Dataset to use for training.")
+    parser.add_argument("-r", "--run_id", type=str, default="", help="Run ID for the experiment.")
     parser.add_argument("-p", "--percentage", type=float, default=1, help="Percentage of data to sample for training, validation, and testing.")
+    parser.add_argument("-c", "--config", type=str, default="config.txt", help="Path to the config file.")
+    parser.add_argument("-s", "--save_dir", type=str, default="vector_cache", help="Path to the save directory.")
+    parser.add_argument("-f", "--feature_dim", type=int, default=617, help="Feature dimension.")
+    parser.add_argument("-n", "--normalized", type=bool, default=False, help="Whether to use normalized vectors.")
+    parser.add_argument("-l", "--language", type=str, default="en", choices=["en", "ru"], help="Language of the dataset.")
     args = parser.parse_args()
 
-    g2v_vectorizer = Gram2VecModule(dataset=args.dataset)
+    vectorizer_configs = {
+        "pos_unigrams":1,
+        "pos_bigrams":1,
+        "func_words":1,
+        "punctuation":1,
+        "letters":0,
+        "emojis":1,
+        "dep_labels":1, 
+        "morph_tags":1,
+        "sentences":1,
+        "num_tokens":0 
+    }
+
+    if args.language == "en":
+        train_df = pd.read_csv(f"../data/{args.dataset}/train.csv", encoding="utf-8")
+        # train_df = pd.read_csv("../data/hiatus_combined_hard_negative/train.csv", encoding="utf-8")
+        dev_df = pd.read_csv(f"../data/{args.dataset}/dev.csv", encoding="utf-8")
+        test_df = pd.read_csv(f"../data/{args.dataset}/test.csv", encoding="utf-8")
+        os.environ["LANGUAGE"] = "en"
+        os.environ["SPACY_MODEL"] = "en_core_web_lg"
+    elif args.language == "ru":
+        train_df = pd.read_csv(f"../data/{args.dataset}/russian/train.csv", encoding="utf-8")
+        dev_df = pd.read_csv(f"../data/{args.dataset}/russian/dev.csv", encoding="utf-8")
+        test_df = pd.read_csv(f"../data/{args.dataset}/russian/test.csv", encoding="utf-8")
+        os.environ["LANGUAGE"] = "ru"
+        os.environ["SPACY_MODEL"] = "ru_core_news_lg"
+        args.feature_dim = 613
+    
+    from explainable_module import Gram2VecModule
+    g2v_vectorizer = Gram2VecModule(filepath=f"vector_cache/{args.dataset}_{args.run_id}_vector_map.pkl", dataset=args.dataset, save_dir=args.save_dir, run_id=args.run_id, configs=vectorizer_configs)
+    # if not os.path.exists(f"vector_cache/{args.dataset}_{args.run_id}_normalized_vector_map.pkl"):
+        # g2v_vectorizer.normalize_cache()
+        # g2v_vectorizer.save_cache(normalized=True)
 
     if args.model_type == "luar":
         model_type = "rrivera1849/LUAR-MUD"
+    elif args.model_type == "luar-ru":
+        model_type = "/home/zengpe/rsp/LUAR-RU"
     elif args.model_type == "roberta":
         model_type = "sentence-transformers/all-distilroberta-v1"
     elif args.model_type == "roberta-large":
         model_type = "sentence-transformers/all-roberta-large-v1"
+    elif args.model_type == "style":
+        model_type = "AnnaWegmann/Style-Embedding"
 
-    model = AttentionResidualModel(model_type=model_type)
+    model = AttentionResidualModel(model_type=model_type, feature_dim=args.feature_dim)
 
     model.to(device)
     
-    train_df = pd.read_csv(f"../data/{args.dataset}/train.csv", encoding="utf-8", lineterminator='\n')
-    dev_df = pd.read_csv(f"../data/{args.dataset}/dev.csv", encoding="utf-8", lineterminator='\n')
-    test_df = pd.read_csv(f"../data/{args.dataset}/test.csv", encoding="utf-8", lineterminator='\n')
-
     if args.percentage != 1:
         train_df_sampled = train_df.sample(frac=args.percentage, random_state=30)  # random_state ensures reproducibility
         dev_df_sampled = dev_df.sample(frac=args.percentage, random_state=30)  # random_state ensures reproducibility
@@ -65,24 +131,26 @@ if __name__ == "__main__":
         dev_data = process_posts(dev_df_sampled, g2v_vectorizer)
         test_data = process_posts(test_df_sampled, g2v_vectorizer)
     else:
-        train_data = process_posts(train_df, g2v_vectorizer)
-        dev_data = process_posts(dev_df, g2v_vectorizer)
-        test_data = process_posts(test_df, g2v_vectorizer)
+        train_data = process_posts(train_df, g2v_vectorizer, normalized=args.normalized)
+        dev_data = process_posts(dev_df, g2v_vectorizer, normalized=args.normalized)
+        test_data = process_posts(test_df, g2v_vectorizer, normalized=args.normalized)
         test_df_sampled = test_df
 
-    # if args.percentage == 1:  
-        # g2v_vectorizer.save_cache()
+    if args.percentage == 1:
+        # g2v_vectorizer.normalize_cache()
+        g2v_vectorizer.save_cache()
 
+    print(model_type)
     train_dataset = AttentionResidualDataset(train_data, model_type = model_type)
     dev_dataset = AttentionResidualDataset(dev_data, model_type = model_type)
     test_dataset = AttentionResidualDataset(test_data, model_type = model_type)
 
-    # Optimized for A6000 GPU
-    if args.model_type == 'roberta-large' or args.model_type == 'longformer':
+    # Optimized for A6000 GPU 
+    if args.model_type == "roberta-large":
         b_size = 16
-    elif args.model_type == 'roberta' or args.model_type == 'style':
+    elif args.model_type == "style":
         b_size = 32
-    elif args.model_type == 'luar':
+    elif args.model_type == 'luar' or args.model_type == "roberta":
         b_size = 64
     
     accumulation_steps = 1
@@ -129,7 +197,8 @@ if __name__ == "__main__":
                 optimizer.step()  # Update parameters
                 optimizer.zero_grad()  # Reset gradients for the next set of accumulation steps
 
-            total_loss += loss.item() * accumulation_steps  # Undo the normalization for logging
+            # Detach the loss when accumulating
+            total_loss += loss.detach().item() * accumulation_steps
             train_dataloader.set_postfix(loss=(total_loss / (i + 1)), refresh=False)
             # optimizer.step()
 
@@ -215,13 +284,15 @@ if __name__ == "__main__":
 
     pbar.close()
 
+    neural_cosims = generate_neural_feature_map(test_dataloader, model_type)
+
     # TESTING LOOP TO SEE HOW MUCH FINETUNED MODEL CORRECTS GRAM2VEC:
     gram2vec_cosims = []
-    vector_map = pd.read_pickle(f'{args.dataset}_vector_map.pkl')
+    vector_map = pd.read_pickle(f'{args.save_dir}/{args.dataset}_{args.run_id}_vector_map.pkl')
 
     for i, row in test_df_sampled.iterrows():
-        vector1 = vector_map[row['post_1_id']]
-        vector2 = vector_map[row['post_2_id']]
+        vector1 = vector_map[row['doc_id1']]
+        vector2 = vector_map[row['doc_id2']]
         cosim = cosine_similarity(vector1, vector2)
         gram2vec_cosims.append(cosim[0][0])
 
@@ -248,28 +319,6 @@ if __name__ == "__main__":
     residual_cosims = [gram2vec_cosims[i] + predicted_labels[i] for i in range(len(predicted_labels))]
     same_labels = list(test_df_sampled['same'])
     
-    ### ANALYSIS CODE ###
-    # test_data_for_csv = []
-    # # Collect data for CSV
-    # for i in range(len(predicted_labels)):
-    #     test_data_for_csv.append([gram2vec_cosims[i], predicted_labels[i], residual_cosims[i], same_labels[i]])
-
-    # # Define the CSV file path
-    # csv_file_path = f"results/analysis_{args.model_type}_{args.run_id}_{args.dataset}.csv"
-
-    # # Define the header for the CSV file
-    # csv_header = ["gram2vec cosim", "predicted residual", "corrected cosim", 'same']
-
-    # # Write the data to the CSV file
-    # with open(csv_file_path, mode='w', newline='') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(csv_header)
-    #     for row in test_data_for_csv:
-    #         writer.writerow(row)  # Add None for the threshold column
-
-    # print(f"Test results for analysis saved to {csv_file_path}")
-    ### END ANALYSIS CODE ###
-
     ### EVALAUTION CODE ###
     thresholds = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
     for threshold in thresholds:
@@ -309,11 +358,11 @@ if __name__ == "__main__":
         precision_per_class = precision_score(true_labels_np, predicted_labels_residual, average=None, zero_division=0)
         recall_per_class = recall_score(true_labels_np, predicted_labels_residual, average=None, zero_division=0)
 
-        # Define the CSV file path
-        if args.imbalanced:
-            csv_file_path = f"attention_residual_results/imbalanced/results_{args.model_type}_{args.dataset}_{args.run_id}.csv"
-        else: 
-            csv_file_path = f"attention_residual_results/results_{args.model_type}_{args.dataset}_{args.run_id}.csv"
+        # Check if the directory exists, create it if not
+        results_dir = "attention_residual_results"
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        csv_file_path = f"attention_residual_results/results_{args.model_type}_{args.dataset}_{args.run_id}.csv"
 
         # Define the header for the CSV file
         csv_header = [
@@ -358,11 +407,15 @@ if __name__ == "__main__":
     fpr_gram2vec, tpr_gram2vec, _ = roc_curve(true_labels_np, gram2vec_cosims)
     auc_gram2vec = auc(fpr_gram2vec, tpr_gram2vec)
 
+    fpr_neural, tpr_neural, _ = roc_curve(true_labels_np, neural_cosims)
+    auc_neural = auc(fpr_neural, tpr_neural)
+
     # print(f"gram2vec AUC: {auc_gram2vec:.3f}, residual AUC: {auc_residual:.3f}")
     # Plot the ROC curve
     plt.figure()
     plt.plot(fpr_residual, tpr_residual, color='darkorange', lw=2, label=f'Residual AUC = {auc_residual:.3f}')
     plt.plot(fpr_gram2vec, tpr_gram2vec, color='blue', lw=2, label=f'Gram2Vec AUC = {auc_gram2vec:.3f}')
+    plt.plot(fpr_neural, tpr_neural, color='green', lw=2, label=f'Neural AUC = {auc_neural:.3f}')
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
@@ -371,11 +424,11 @@ if __name__ == "__main__":
     plt.title(f'AUC Curve for {args.model_type} on {args.dataset}')
     plt.legend(loc="lower right")
     plt.grid()
-    if args.imbalanced:
-        plt.savefig(f"attention_residual_results/imbalanced/graphs/{args.model_type}_{args.dataset}_{args.run_id}_residual_auc.png")
-    else:
-        plt.savefig(f"attention_residual_results/graphs/{args.model_type}_{args.dataset}_{args.run_id}_residual_auc.png")
-
+    # Check if the directory exists, if not create it
+    save_dir = os.path.dirname("attention_residual_results/graphs/")
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    plt.savefig(f"attention_residual_results/graphs/{args.model_type}_{args.dataset}_{args.run_id}_residual_auc.png")
     # # Check if the CSV file already exists
     file_exists = os.path.isfile(csv_file_path)
 
@@ -405,5 +458,5 @@ if __name__ == "__main__":
     plt.xlabel('Residual Value')
     plt.ylabel('Frequency')
     plt.grid(axis='y', alpha=0.75)
-    plt.show()
-    plt.savefig(f"results/predicted_labels_{args.model_type}_{args.run_id}_{args.dataset}.png")
+
+    plt.savefig(f"attention_residual_results/graphs/predicted_labels_{args.model_type}_{args.run_id}_{args.dataset}.png")
